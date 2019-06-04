5578b33bb38a656ce172c345d1e6044134273c6d
Kyle Taylor <kyletaylored@gmail.com>
2019-06-04 09:07:49 -0500

    Updating setuptools config

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ import setuptools
 
 setuptools.setup(
     name='tsl2591',
-    version='0.0.2',
-    url='http://github.com/maxlklaxl/tsl2591',
+    version='0.0.3',
+    url='http://github.com/maxlklaxl/python-tsl2591',
     author='Max Hofbauer',
     author_email='maxhofb@gmail.com',
     description='Community-coded Python module for TSL2591 sensor. Use at your own risk.',
+    license="MIT",
     packages=setuptools.find_packages(),
     long_description=open('README.rst').read(),
     install_requires=['smbus-cffi'],
+    py_modules=['tsl2591'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 2",
